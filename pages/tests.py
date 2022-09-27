442c59bcb5f131ec63b0f6c2741e0157522fe4b4
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-09-27 20:17:00 +0100

    tested out two pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+
+
+class HomePageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+class AboutPageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
