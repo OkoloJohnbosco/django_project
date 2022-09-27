b9a53d07debac51f5ee3b1f126b88b72c21beea4
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-09-27 21:12:16 +0100

    improved the tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,4 +1,5 @@
 from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
 
@@ -8,8 +9,32 @@ class HomePageTests(SimpleTestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        res = self.client.get(reverse("home"))
+        self.assertTemplateUsed(res, "home.html")
+
+    def test_template_content(self):
+        res = self.client.get(reverse("home"))
+        self.assertContains(res, "<h1>Homepage</h1>")
+
 
 class AboutPageTests(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/about/")
         self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        res = self.client.get(reverse("about"))
+        self.assertTemplateUsed(res, "about.html")
+
+    def test_template_content(self):
+        res = self.client.get(reverse("about"))
+        self.assertContains(res, "<h1>About Page</h1>")
