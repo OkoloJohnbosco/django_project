0a316332e9b8a640aac600723e02f0c1e7bfb84d
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-09-27 19:41:28 +0100

    created the about page using CBV

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homePageView
+from .views import AboutPageView, HomePageView
 
 urlpatterns = [
-    path("", homePageView, name="home")
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about")
 ]
